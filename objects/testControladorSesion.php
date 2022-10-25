87a606083533337886f99c3834baf1898b20ec14
nehuengiacone <113162080+nehuengiacone@users.noreply.github.com>
2022-10-24 21:01:51 -0300

    Delete testControladorSesion.php

--- objects/testControladorSesion.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once "ControladorSesion.php";
-require_once "RepositorioInstitucion.php";
-
-$controlador = new ControladorSesion();
-$repositorio = new RepositorioInstitucion();
-
-$controlador->validarCredencialesInstitucion("Urquiza", "1234");
-//OK
